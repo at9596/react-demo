226c6798322a9511ffbf4efc606ab4688c31e134
abhishek Tanwar <abhishekgangdhari@gmail.com>
2026-05-15 08:06:48 +0530

    feat: add react

---
+++ app/javascript/entrypoints/application.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+
+function App() {
+  return (
+    <div>
+      <h1>Hello React + Rails + Vite 🚀</h1>
+    </div>
+  )
+}
+
+ReactDOM.createRoot(
+  document.getElementById("root")
+).render(<App />)
